b3ebaf8190aa809a3d22ecdfc7e673997516b1e0
Vlad Blazhko <mr_plum@mail.ru>
2019-03-03 18:05:50 +0300

    Implement firebase registration

--- js/registration.js
+++ js/registration.js
@@ -1,31 +1,54 @@
-function register() { 
-    var participantObject = {
-        first_name: "a",
-        last_name: "b",
-        sex: "Female",
-        email: "kek",
-        telephone: "mom",
-        organization: "asd",
-        address: "address",
-        invoice_address: "invoice",
-        fee: 30,
-        lunch_monday: true,
-        lunch_tuesday: true,
-        lunch_wednesday: true,
-        lunch_vegan: true,
-        dinner_tuesday: true,
-        dinner_vegan: true,
-        minsk_tour: true
+document.getElementById('registration-form').addEventListener('submit', register);
+function register(e) { 
+    e.preventDefault();
 
+    first_name = getInputValueById('registration-first-name');
+    last_name = getInputValueById('registration-last-name');
+
+    fee_type = document.querySelector('input[name="fees"]:checked').value;
+    fee = 0;
+    switch (fee_type){
+        case 'eu-student': fee = 30; break;
+        case 'non-eu-student': fee = 60; break;
+        case 'academic': fee = 100; break;
+        case 'non-academic': fee = 150; break;
+    }
+    
+    var participantObject = {
+        sex: document.querySelector('input[name="sex"]:checked').value,
+        first_name: first_name,
+        last_name: last_name,
+
+        email: getInputValueById('registration-email'),
+        telephone: getInputValueById('registration-telephone'),
+
+        organization: getInputValueById('registration-organization'),
+        address: getInputValueById('registration-address'),
+        invoice_address: getInputValueById('registration-invoice-address'),
+        
+        fee_type: fee_type,
+        fee: fee,
+        
+        lunch_monday: getCheckedById('registration-lunch-mon'),
+        lunch_tuesday: getCheckedById('registration-lunch-tue'),
+        lunch_wednesday: getCheckedById('registration-lunch-wed'),
+        lunch_vegetarian: getCheckedById('registration-lunch-veg'),
+        
+        dinner_tuesday: getCheckedById('registration-dinner-tue'),
+        dinner_vegetarian: getCheckedById('registration-dinner-veg'),
+
+        food_special: getInputValueById('registration-food-special'),
+        
+        minsk_tour: getCheckedById('registration-minsk-tour')
     };
 
-    console.log('ekk');
     firestore.collection('participants').add(participantObject)
         .then(function(snapshot) {
-            console.log('success');
+            M.toast({html: 'Dear ' + first_name + ' ' + last_name + ', You are successfully registered!'});
+            document.getElementById('registration-form').reset();
         }, function(error) {
-            console.log('error' + error);
-            // error(); // some error method
+            console.log('Registration error ' + error);
+            M.toast({html: 'The error ' + error + ' happens. <br><br> Please, contact us by email biospec-workshop@nmbu.no'});
         });
 
     return false;
@@ -50,7 +73,7 @@ function subscribe(e) {
             M.toast({html: 'Dear ' + first_name + ' ' + last_name + ', You are successfully subscribed!'});
             document.getElementById('subscribe-form').reset();
         }).catch(function(error) {
-            console.log('Subscription error ' + error)
+            console.log('Subscription error ' + error);
             M.toast({html: 'The error ' + error + ' happens. <br><br> Please, contact us by email biospec-workshop@nmbu.no'});
         });
     
@@ -59,4 +82,8 @@ function subscribe(e) {
 
 function getInputValueById(id){
     return document.getElementById(id).value;
+}
+
+function getCheckedById(id){
+    return document.getElementById(id).checked;
 }
