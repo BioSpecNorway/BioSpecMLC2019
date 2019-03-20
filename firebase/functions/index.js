fbcfddca292cbe525c01466c7bd158c196304e46
Vlad Blazhko <mr_plum@mail.ru>
2019-03-20 07:37:08 +0300

    Add registration function to firebase

--- firebase/functions/index.js
+++ firebase/functions/index.js
@@ -18,6 +18,14 @@ const mailTransport = nodemailer.createTransport({
   },
 });
 
+exports.sendRegisterEmail = functions.firestore.document('participants/{participantId}')
+  .onCreate((snap, context) => {
+    const subscriber = snap.data();
+
+    notifyAboutNewParticipant(subscriber, 'biospec-workshop@nmbu.no');
+    return confirmRegistration(subscriber);
+  });
+
 exports.sendSubscribeEmail = functions.firestore.document('subscribers/{subscriberId}')
   .onCreate((snap, context) => {
     const subscriber = snap.data();
@@ -25,6 +33,95 @@ exports.sendSubscribeEmail = functions.firestore.document('subscribers/{subscrib
     notifyAboutNewSubscriber(subscriber, 'biospec-workshop@nmbu.no');
     return confirmSubscription(subscriber);
   });
+
+function notifyAboutNewParticipant(participant, receiver_email) {
+  const mailOptions = {
+    from: '"BioSpecMLC 2019" <noreply@firebase.com>',
+    to: receiver_email,
+  };
+
+  // Building Email message.
+  mailOptions.subject = 'Registration notification';
+  mailOptions.text = 'New participant: ' + participant.first_name +
+    ' ' + participant.last_name + ' ' + participant.email;
+
+  return mailTransport.sendMail(mailOptions).then(() => {
+    return console.log('Notification email sent to:', mailOptions.to);
+  }).catch((error) => {
+    return console.error('There was an error while sending the email:', error);
+  });
+}
+
+function confirmRegistration(participant) {
+  const mailOptions = {
+    from: '"BioSpecMLC 2019" <noreply@firebase.com>',
+    to: participant.email,
+  };
+
+  // Building Email message.
+  mailOptions.subject = 'Registration confirmation';
+  mailOptions.text = 'Dear ' + participant.first_name + ' ' + participant.last_name + ', \n\n' +
+    `
+    You are successfully registered at BioSpecMLC 2019 (https://biospecnorway.github.io/BioSpecMLC2019)! \n
+    \n
+    Your registration will be accepted after payment.\n
+    \n
+    PAYMENT METHOD \n
+    -------------------------------------------------\n
+    Please, find this information at website https://biospecnorway.github.io/BioSpecMLC2019 \n
+    \n
+    You can contact us by email biospec-workshop@nmbu.no
+    `;
+  mailOptions.html = 'Dear ' + participant.first_name + ' ' + participant.last_name + ', <br><br>' +
+    `You are successfully registered at <a href="https://biospecnorway.github.io/BioSpecMLC2019/" target="_blank">BioSpecMLC 2019</a>! <br>
+    <br>
+    Your registration will be accepted after payment.
+
+    <h5> Payment Method </h5>
+
+    Payments should be made by bank transfer at:
+
+    <table>
+      <tbody>
+        <tr>
+          <td> Name </td>
+          <td>DNB Bank ASA </td>
+        </tr>
+        <tr>
+          <td> Account owner </td>
+          <td> Norwegian University of Life Sceinces </td>
+        </tr>
+        <tr>
+          <td> Bank code </td>
+          <td> DNB </td>
+        </tr>
+        <tr>
+          <td> SWIFT code </td>
+          <td> DNBANOKK </td>
+        </tr>
+        <tr>
+          <td> Account number </td>
+          <td> 76940512510 </td>
+        </tr>
+        <tr>
+          <td> IBAN </td>
+          <td> NO8976940512510 </td>
+        </tr>
+      </tbody>
+    </table>
+
+    <p style="color: red"> Note the payment with reference number: 1650026066 </p>
+    <br>
+    For more information check <a href="https://biospecnorway.github.io/BioSpecMLC2019/" target="_blank">workshop website</a>. <br>
+    For any questions you can contact us by email <b>biospec-workshop@nmbu.no</b>
+    `;
+
+  return mailTransport.sendMail(mailOptions).then(() => {
+    return console.log('Registration confirmation sent to:', mailOptions.to);
+  }).catch((error) => {
+    return console.error('There was an error while sending the email:', error);
+  });
+}
 
 function notifyAboutNewSubscriber(subscriber, receiver_email) {
   const mailOptions = {
@@ -53,8 +150,8 @@ function confirmSubscription(subscriber) {
   // Building Email message.
   mailOptions.subject = 'Subscription confirmation';
   mailOptions.text = 'Dear ' + subscriber.first_name + ' ' + subscriber.last_name + ', \n\n' +
-  'Thank you for subscribing to BioSpecMLC 2019 (https://biospecnorway.github.io/BioSpecMLC2019)! \n\n' +
-  'You can contact us by email biospec-workshop@nmbu.no';
+    'Thank you for subscribing to BioSpecMLC 2019 (https://biospecnorway.github.io/BioSpecMLC2019)! \n\n' +
+    'You can contact us by email biospec-workshop@nmbu.no';
   mailOptions.html = 'Dear ' + subscriber.first_name + ' ' + subscriber.last_name + ', <br><br>' +
     'Thank you for subscribing to <a href="https://biospecnorway.github.io/BioSpecMLC2019/" target="_blank">BioSpecMLC 2019</a>! <br><br>' +
     'You can contact us by email <b>biospec-workshop@nmbu.no</b>';
